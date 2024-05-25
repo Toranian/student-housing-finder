1d4e3b3110ac1a0d39315225cd8da16a3155ae51
Isaac Morrow <isaacmorrowofficial@gmail.com>
2024-05-25 12:47:38 -0700

    feat: Added landing page, daisyui & light theme

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import DeployButton from "../components/DeployButton";
+import Logo from "../components/Logo";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
@@ -23,17 +23,13 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
+          <Logo />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
@@ -45,7 +41,7 @@ export default async function Index() {
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            RedBull
           </a>
         </p>
       </footer>
--- frontend/components/DeployButton.tsx
+++ frontend/components/DeployButton.tsx
@@ -1,4 +1,4 @@
-export default function DeployButton() {
+export default function Logo() {
   return (
     <a
       className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border"
