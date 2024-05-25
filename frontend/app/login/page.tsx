20ec997465d831fa993ddb0bc6c6a3b6e8237632
Isaac Morrow <isaacmorrowofficial@gmail.com>
2024-05-25 16:11:55 -0700

    feat: Updated login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -74,7 +74,10 @@ export default function Login({
         Back
       </Link>
 
+
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+      <h1 className="text-xl md:text-2xl font-semibold">Login</h1>
+      <p className="mb-3">Find your perfect rental, sublet, or roommate.</p> 
         <label className="text-md" htmlFor="email">
           Email
         </label>
@@ -96,7 +99,7 @@ export default function Login({
         />
         <SubmitButton
           formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="bg-emerald-500 rounded-md px-4 py-2 text-foreground mb-2"
           pendingText="Signing In..."
         >
           Sign In
