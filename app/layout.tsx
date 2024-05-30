bbc82649fef0d4ebe22d9bf27ce6f479c19255d2
Isaac Morrow <isaacmorrowofficial@gmail.com>
2024-05-30 13:15:43 -0700

    feat: Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Room Radar",
+  description: "Find your perfect rental, sublet, or roommate.",
+  image: "./thumbnail.jpg",
 };
 
 export default function RootLayout({
